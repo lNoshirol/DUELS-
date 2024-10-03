4831e03bb0f39bab95bf2dad024bfb2a6db249dc
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-10-03 09:55:10 +0200

    tonk

---
+++ Duel d'Argann/Assets/Script/Control.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField] bool isStickUse;
+    [SerializeField] float speed;
+
+    [SerializeField] private Bounds moovementLimits;
+    private Vector3 moovement;
+    [SerializeField] private float jspAVecLeSida;
+    [SerializeField] private float  turretOrientationValue;
+
+    [SerializeField] GameObject turretObject;
+
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.started)
+        {
+            isStickUse = true;
+            Debug.Log("STAR MOVE");
+        }
+        if (callbackContext.canceled)
+        {
+            isStickUse = false;
+        }
+        Vector2 orientation = callbackContext.ReadValue<Vector2>();
+        //Debug.Log(orientation);
+        moovement = new Vector3(orientation.x, 0, 0);
+    }
+
+    public void OnRotateTurret(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.started && turretOrientationValue >= 0)
+        {
+            if (callbackContext.ReadValue<Vector2>().y > 0)
+            {
+                jspAVecLeSida = 1;
+            }
+            else
+            {
+                jspAVecLeSida = -1;
+            }
+
+        }
+        if (callbackContext.canceled)
+        {
+            jspAVecLeSida = 0;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStickUse)
+        {
+            transform.position += (moovement * speed * Time.deltaTime);
+            transform.position = moovementLimits.ClosestPoint(transform.position);
+        }
+
+        if (jspAVecLeSida != 0 && 900 >= turretOrientationValue && turretOrientationValue >= 0)
+        {
+            turretOrientationValue += jspAVecLeSida * Time.deltaTime;
+            turretObject.transform.Rotate(new Vector3(0, 0, jspAVecLeSida));
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawWireCube(moovementLimits.center, moovementLimits.size);
+    }
+}
