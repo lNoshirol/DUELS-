550a31d3d39f12151de7081fd5536125107b7aa7
Asae <n.noahchery@gmail.com>
2024-10-04 11:04:23 +0200

    git error de merde

--- Duel d'Argann/Assets/Script/TagGameScript/PlayerTouch.cs
+++ Duel d'Argann/Assets/Script/TagGameScript/PlayerTouch.cs
@@ -7,20 +7,27 @@ public class PlayerTouch : MonoBehaviour
 
     [SerializeField] private string _tag;
     [SerializeField] private GameObject _bomb;
+    [SerializeField] private float _inpulsionForce;
+    [SerializeField] private Vector2 _inpulsionOrientation;
+    private Rigidbody2D _rb;
 
-
-    void Start()
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
-
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == _tag)
         {
             Debug.Log("Touché !");
+<<<<<<< Updated upstream
             if (_bomb.activeInHierarchy) _bomb.SetActive(false); else _bomb.SetActive(true);//suce
+=======
+            if (_bomb.activeInHierarchy) _bomb.SetActive(false); else _bomb.SetActive(true);
+            _rb.AddForce(-other.relativeVelocity * _inpulsionForce, ForceMode2D.Impulse);
+
+>>>>>>> Stashed changes
         }
     }
 }
